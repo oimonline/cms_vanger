e646ec355eab4bfe8264205e2b95b70df1d34416
oimonline <oimonline@gmail.com>
2012-06-25 22:18:48 +0400

    commit

--- faq/models.py
+++ faq/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Question(models.Model):
+    text = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
+
+class Answer(models.Model):
+    text = models.CharField(max_length=200)
+    author = models.CharField(max_length=200)
